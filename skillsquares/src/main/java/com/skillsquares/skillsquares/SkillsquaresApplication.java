b2b816b94b86508b4324da33c7a1fcead747cd37
RetinaNisha2004 <146425683+RetinaNisha2004@users.noreply.github.com>
2026-06-19 12:23:24 +0530

    initial
    initial

---
+++ skillsquares/src/main/java/com/skillsquares/skillsquares/SkillsquaresApplication.java
@@ -0,0 +1,13 @@
+package com.skillsquares.skillsquares;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillsquaresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillsquaresApplication.class, args);
+	}
+
+}
